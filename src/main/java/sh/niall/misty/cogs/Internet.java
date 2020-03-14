8b31d312349837a87a7465a9e3a71d263e36b4d4
Niall <50669114+niallsh@users.noreply.github.com>
2020-03-14 01:41:19 +0000

    Created fox command

--- src/main/java/sh/niall/misty/cogs/Internet.java
+++ src/main/java/sh/niall/misty/cogs/Internet.java
@@ -28,6 +28,7 @@ public class Internet extends Cog {
     OkHttpClient client = new OkHttpClient();
     final String[] dogChoices = {"🐶 Woof! 🐶", "🐶 Bark! 🐶", "🐶 Arf! 🐶"};
     final String[] catChoices = {"\uD83D\uDC31 Meow! \uD83D\uDC31", "\uD83D\uDC31 Purr! \uD83D\uDC31"};
+    final String[] foxChoices = {"\uD83E\uDD8A Wa-pa-pa-pa-pa-pa-pow! \uD83E\uDD8A", "\uD83E\uDD8A Hatee-hatee-hatee-ho! \uD83E\uDD8A", "\uD83E\uDD8A Joff-tchoff-tchoffo-tchoffo-tchoff! \uD83E\uDD8A", "\uD83E\uDD8A Fraka-kaka-kaka-kaka-kow! \uD83E\uDD8A"};
     final DateTimeFormatter urbanInput = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @Command(name = "dog", aliases = {"puppo", "puppos"})
@@ -95,6 +96,31 @@ public class Internet extends Cog {
         embedBuilder.setDescription(catChoices[(int) (Math.random() * catChoices.length)]);
         embedBuilder.setColor(Helper.randomColor());
         embedBuilder.setImage((String) jsonObject.get("url"));
+        context.send(embedBuilder.build());
+    }
+
+    @Command(name = "fox")
+    public void _commandFox(Context context) throws IOException, CommandException {
+        context.getChannel().sendTyping().queue();
+
+        // Request a resource
+        Response resourceRequest = client.newCall(new Request.Builder().url("https://randomfox.ca/floof").build()).execute();
+
+        // Ensure we got through
+        if (resourceRequest.code() != 200) {
+            resourceRequest.close();
+            throw new CommandException("There were no available foxes to photograph!");
+        }
+
+        // Covert string to JSON
+        JSONObject jsonObject = new JSONObject(resourceRequest.body().string());
+
+        // Create the embed
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle("Fox Photo!");
+        embedBuilder.setDescription(foxChoices[(int) (Math.random() * foxChoices.length)]);
+        embedBuilder.setColor(Helper.randomColor());
+        embedBuilder.setImage((String) jsonObject.get("image"));
         context.send(embedBuilder.build());
     }
 
